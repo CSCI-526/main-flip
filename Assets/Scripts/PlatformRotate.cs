9e7c2b079d16b3612d330d23d50fe52e8f137395
Yang Li <lyang835@usc.edu>
2025-10-14 20:29:01 -0700

    new map design

---
+++ Assets/Scripts/PlatformRotate.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class PlatformRotate : MonoBehaviour
+{
+
+    public float rotationSpeed = 10f;
+    private Rigidbody2D rb;
+    private bool isRotating = false;
+
+    // Initialize the rotating platform
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.bodyType = RigidbodyType2D.Kinematic;
+        rb.useFullKinematicContacts = true;
+        rb.interpolation = RigidbodyInterpolation2D.Interpolate;
+
+        var col = GetComponent<Collider2D>();
+        if (col) col.isTrigger = false;
+    }
+
+    // FixedUpdate is called when triggered, to make the platform rotate.
+    void FixedUpdate()
+    {
+        if (!isRotating) return;
+        rb.MoveRotation(rb.rotation + rotationSpeed * Time.fixedDeltaTime);
+    }
+
+    public void Activate()
+    {
+        isRotating = true;
+    }
+    
+}
---
+++ Assets/Scripts/RotateTrigger.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class RotateTrigger : MonoBehaviour
+{
+
+    public PlatformRotate platform;
+    private bool isTriggered = false;
+
+    // To make sure the collider is a trigger
+    void Reset()
+    {
+        var col = GetComponent<Collider2D>();
+        if (col) col.isTrigger = true;
+    }
+
+    // When the player triggers, platform starts rotating and the trigger is destroyed.
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (isTriggered) return;
+        if (other.CompareTag("Player"))
+        {
+            isTriggered = true;
+            platform.Activate();
+            Destroy(gameObject);
+        }
+        
+    }
+}
