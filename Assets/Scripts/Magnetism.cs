f8fd1b9288f8c4dc9b5002a96c97e818af169737
unknown <zhangjiayi1512@outlook.com>
2025-10-15 23:30:48 -0700

    add magnetic effects to the map

--- Assets/Scripts/Magnetism.cs
+++ Assets/Scripts/Magnetism.cs
@@ -1,139 +1,3 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-
-// public enum MagnetismAxis
-// {
-//     None,
-//     X,
-//     Y
-// }
-
-// public enum MagnetismMode
-// {
-//     Radial,
-//     Axial
-// }
-
-// public enum MagneticPole
-// {
-//     None,
-//     North,
-//     South
-// }
-
-// public class Magetism : MonoBehaviour
-// {
-//     public float magetismStrength = 20f;
-//     public MagneticPole currentPole;
-//     public MagnetismMode currentMode;
-//     public MagnetismAxis currentAxis;
-//     public float attractionRange = 5f;
-
-//     private List<GameObject> collidingMagnets = new List<GameObject>();
-
-//     void Update()
-//     {
-//         UpdateMagneticField();
-//         UpdateColor();
-//     }
-
-//     void UpdateMagneticField()
-//     {
-//         Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, attractionRange);
-//         foreach (Collider2D other in colliders)
-//         {
-//             if (other.gameObject == this.gameObject) continue; // Skip self
-
-//             Magetism otherMagetism = other.GetComponent<Magetism>(); // Skip non-magnetic objects
-//             if (otherMagetism != null && otherMagetism.currentPole != MagneticPole.None && currentPole != MagneticPole.None)
-//             {
-//                 Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
-//                 if (rb != null)
-//                 {
-//                     Vector2 direction = (Vector2)transform.position - rb.position;
-//                     MagnetismMode effectiveMode = currentMode;
-//                     if (collidingMagnets.Contains(other.gameObject))
-//                     {
-//                         effectiveMode = MagnetismMode.Radial;
-//                     }
-//                     if (effectiveMode == MagnetismMode.Axial)
-//                     {
-//                         if (currentAxis == MagnetismAxis.None)
-//                         {
-//                             if (direction.x > direction.y)
-//                             {
-//                                 direction.x = 0; // Ignore X axis
-//                             }
-//                             else
-//                             {
-//                                 direction.y = 0; // Ignore Y axis
-//                             }
-//                         }
-//                         else if (currentAxis == MagnetismAxis.X)
-//                         {
-//                             direction.y = 0; // Ignore Y axis
-//                         }
-//                         else if (currentAxis == MagnetismAxis.Y)
-//                         {
-//                             direction.x = 0; // Ignore X axis
-//                         }
-//                     }
-
-//                     if (currentPole == otherMagetism.currentPole)
-//                     {
-//                         //Repulsion
-//                         rb.AddForce(-direction.normalized * magetismStrength);
-//                     }
-//                     else
-//                     {
-//                         //Attraction
-//                         rb.AddForce(direction.normalized * magetismStrength);
-//                     }
-//                 }
-//             }
-
-//         }
-//     }
-
-//     void UpdateColor()
-//     {
-//         if (this.currentPole == MagneticPole.North)
-//         {
-//             this.GetComponent<SpriteRenderer>().color = Color.red;
-//         }
-//         else if (this.currentPole == MagneticPole.South)
-//         {
-//             this.GetComponent<SpriteRenderer>().color = Color.blue;
-//         }
-//         else
-//         {
-//             this.GetComponent<SpriteRenderer>().color = Color.black;
-//         }
-//     }
-
-//     void OnCollisionEnter2D(Collision2D collision)
-//     {
-//         Magetism otherMagetism = collision.gameObject.GetComponent<Magetism>();
-//         if (otherMagetism != null && !collidingMagnets.Contains(collision.gameObject))
-//         {
-//             collidingMagnets.Add(collision.gameObject);
-//         }
-//     }
-    
-//     void OnCollisionExit2D(Collision2D collision)
-//     {
-//         Magetism otherMagetism = collision.gameObject.GetComponent<Magetism>();
-//         if (otherMagetism != null)
-//         {
-//             collidingMagnets.Remove(collision.gameObject);
-//         }
-//     }
-// }
-
-        
-
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -158,66 +22,67 @@ public enum MagneticPole
     South
 }
 
-public class Magetism : MonoBehaviour
+public class Magnetism : MonoBehaviour
 {
-    public float magetismStrength = 100f;
+    public float maxMagnetismStrength = 100f;
+    public float minMagnetismStrength = 100f;
+    public bool changeableMode = true;
     public MagneticPole currentPole;
     public MagnetismMode currentMode;
     public MagnetismAxis currentAxis;
-    public float attractionRange = 5f;
 
+    private List<Rigidbody2D> attractedObjects = new List<Rigidbody2D>();
     private List<GameObject> collidingMagnets = new List<GameObject>();
 
-    void Update()
+    void FixedUpdate()
     {
-        UpdateMagneticField();
+        UpdateMagneticForce();
         UpdateColor();
     }
 
-    void UpdateMagneticField()
+    void UpdateMagneticForce()
     {
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, attractionRange);
-        foreach (Collider2D other in colliders)
-        {
-            if (other.gameObject == this.gameObject) continue; // Skip self
+        if (currentPole == MagneticPole.None) return;
 
-            // Only affect the player
-            if (!other.CompareTag("Player")) continue;
+        foreach (Rigidbody2D rb in attractedObjects) {
+            if (rb == null) continue; // skip null references
+            if (rb.gameObject == this.gameObject) continue; // skip self
+            if (!rb.CompareTag("Player")) continue; // only affect the player
 
-            Magetism otherMagetism = other.GetComponent<Magetism>();
-            if (otherMagetism != null && otherMagetism.currentPole != MagneticPole.None && currentPole != MagneticPole.None)
-            {
-                Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
-                if (rb != null)
+            Magnetism otherMagnetism = rb.GetComponent<Magnetism>();
+            if (otherMagnetism != null && otherMagnetism.currentPole != MagneticPole.None && currentPole != MagneticPole.None) {
+                Vector2 direction = (Vector2)transform.position - rb.position;
+                MagnetismMode effectiveMode = currentMode;
+
+                
+                if (collidingMagnets.Contains(rb.gameObject) && changeableMode)
                 {
-                    Vector2 direction = (Vector2)transform.position - rb.position;
-                    MagnetismMode effectiveMode = currentMode;
-
-                    if (collidingMagnets.Contains(other.gameObject))
-                    {
-                        effectiveMode = MagnetismMode.Radial;
-                    }
-
-                    if (effectiveMode == MagnetismMode.Axial)
-                    {
-                        if (currentAxis == MagnetismAxis.None)
-                        {
-                            if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
-                                direction.x = 0;
-                            else
-                                direction.y = 0;
-                        }
-                        else if (currentAxis == MagnetismAxis.X)
-                            direction.y = 0;
-                        else if (currentAxis == MagnetismAxis.Y)
-                            direction.x = 0;
-                    }
-
-                    if (currentPole == otherMagetism.currentPole)
-                        rb.AddForce(-direction.normalized * magetismStrength);
-                    else
-                        rb.AddForce(direction.normalized * magetismStrength);
+                    effectiveMode = MagnetismMode.Radial;
                 }
+
+                if (effectiveMode == MagnetismMode.Axial)
+                {
+                    if (currentAxis == MagnetismAxis.None)
+                    {
+                        if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+                            direction.x = 0;
+                        else
+                            direction.y = 0;
+                    }
+                    else if (currentAxis == MagnetismAxis.X)
+                        direction.y = 0;
+                    else if (currentAxis == MagnetismAxis.Y)
+                        direction.x = 0;
+                }
+                
+                float currentMagnetismStrength = maxMagnetismStrength;
+                float distance = direction.magnitude;
+                currentMagnetismStrength /= (distance * distance);
+                currentMagnetismStrength = Mathf.Max(minMagnetismStrength, Mathf.Min(currentMagnetismStrength, maxMagnetismStrength));
+                if (currentPole == otherMagnetism.currentPole)
+                    rb.AddForce(-direction.normalized * currentMagnetismStrength, ForceMode2D.Force);
+                else
+                    rb.AddForce(direction.normalized * currentMagnetismStrength, ForceMode2D.Force);
             }
         }
     }
@@ -225,25 +90,53 @@ public class Magetism : MonoBehaviour
     void UpdateColor()
     {
         SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        if (sr == null) return;
+        Color newColor;
         if (currentPole == MagneticPole.North)
-            sr.color = Color.red;
+        {
+            if (ColorUtility.TryParseHtmlString("#E76963", out newColor))
+            {
+                sr.color = newColor;
+            }
+        }
         else if (currentPole == MagneticPole.South)
-            sr.color = Color.blue;
+        {
+            if (ColorUtility.TryParseHtmlString("#79A1D8", out newColor))
+            {
+                sr.color = newColor;
+            }
+        }
         else
+        {
             sr.color = Color.black;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
+        if (rb != null && !attractedObjects.Contains(rb))
+            attractedObjects.Add(rb);
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        Rigidbody2D rb = other.GetComponent<Rigidbody2D>();
+        if (rb != null)
+            attractedObjects.Remove(rb);
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        Magetism otherMagetism = collision.gameObject.GetComponent<Magetism>();
-        if (otherMagetism != null && !collidingMagnets.Contains(collision.gameObject))
+        Magnetism otherMagnetism = collision.gameObject.GetComponent<Magnetism>();
+        if (otherMagnetism != null && !collidingMagnets.Contains(collision.gameObject))
             collidingMagnets.Add(collision.gameObject);
     }
 
     void OnCollisionExit2D(Collision2D collision)
     {
-        Magetism otherMagetism = collision.gameObject.GetComponent<Magetism>();
-        if (otherMagetism != null)
+        Magnetism otherMagnetism = collision.gameObject.GetComponent<Magnetism>();
+        if (otherMagnetism != null)
             collidingMagnets.Remove(collision.gameObject);
     }
 }
