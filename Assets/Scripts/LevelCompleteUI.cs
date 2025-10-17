e6a4e0fadc9f1dbb5407c6309f7afe6b12dd040e
Sherry Wang <sherry.wang1579@gmail.com>
2025-10-16 21:18:12 -0700

    fixed replay button

--- Assets/Scripts/LevelCompleteUI.cs
+++ Assets/Scripts/LevelCompleteUI.cs
@@ -2,44 +2,48 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-using TMPro;
 
 public class LevelCompleteUI : MonoBehaviour
 {
-    private CanvasGroup group;
-    private Button replayButton;
-    private float fadeDuration = 0.25f;
-    private bool pauseOnShow = true;
+    [SerializeField] private CanvasGroup group;
+    [SerializeField] private Button replayButton;
+
+    [SerializeField] private float fadeDuration = 0.15f;
+    [SerializeField] private bool pauseOnShow = true;
+    [SerializeField] private bool hideOnAwake = true;
+
+    void Reset()
+    {
+        if (!group) group = GetComponent<CanvasGroup>();
+        if (!replayButton) replayButton = GetComponentInChildren<Button>(true);
+    }
 
     void Awake()
     {
         if (!group) group = GetComponent<CanvasGroup>();
+        if (!replayButton) replayButton = GetComponentInChildren<Button>(true);
         if (replayButton) replayButton.onClick.AddListener(OnReplayClicked);
-        HideImmediate();
+
+        if (hideOnAwake) HideImmediate();
     }
 
     public void Show()
     {
         gameObject.SetActive(true);
         StartCoroutine(FadeCanvas(1f));
-
-        if (pauseOnShow)
-            Time.timeScale = 0f;
+        if (pauseOnShow) Time.timeScale = 0f;
     }
-    
+
     public void Hide()
     {
         StartCoroutine(FadeCanvas(0f));
-        if (pauseOnShow)
-            Time.timeScale = 1f;
+        if (pauseOnShow) Time.timeScale = 1f;
     }
 
     private void OnReplayClicked()
     {
-        if (pauseOnShow)
-            Time.timeScale = 1f;
-
-        Scene current = SceneManager.GetActiveScene();
+        if (pauseOnShow) Time.timeScale = 1f;
+        var current = SceneManager.GetActiveScene();
         SceneManager.LoadScene(current.buildIndex);
     }
 
@@ -67,11 +71,10 @@ public class LevelCompleteUI : MonoBehaviour
 
         while (t < fadeDuration)
         {
-            t += Time.unscaledDeltaTime; 
+            t += Time.unscaledDeltaTime;
             group.alpha = Mathf.Lerp(start, target, t / fadeDuration);
             yield return null;
         }
-
         group.alpha = target;
 
         if (Mathf.Approximately(target, 0f))
