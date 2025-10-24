85ecbb9135c2fbce8cbf7fa3d4ee57ada325d510
unknown <zhangjiayi1512@outlook.com>
2025-10-24 01:03:19 -0700

    add FoVFocusAnywhere

---
+++ Assets/Scripts/FoVFocusAnywhere.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class FoVFocusAnywhere : MonoBehaviour
+{
+    public Camera cam;
+    public Transform target;
+    public float moveSpeed = 2f;
+    private Vector3 originalPosition;
+    public Vector3 newPosition;
+    private bool isFocused = false;
+
+    void Start()
+    {
+        originalPosition = cam.transform.position;
+        newPosition.z = originalPosition.z;
+    }
+
+    private void Update()
+    {
+        if (isFocused)
+        {
+            cam.transform.position = Vector3.Lerp(cam.transform.position, newPosition, moveSpeed * Time.deltaTime);       
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.transform == target)
+        {
+            isFocused = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.transform == target)
+        {
+            isFocused = false;
+        }
+    }    
+}
