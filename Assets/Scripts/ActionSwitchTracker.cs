b585d991e83051956633c4e1fc84faeafd78d6bd
xinluxu <xinluxu@usc.edu>
2025-11-13 18:36:04 -0800

    modify ActionSwitchTracker.cs: no need to save csv file, send data to google form directly

--- Assets/Scripts/ActionSwitchTracker.cs
+++ Assets/Scripts/ActionSwitchTracker.cs
@@ -1,11 +1,9 @@
-using System.Collections.Generic;
-using System.IO;
-using System.Globalization;
-using System.Linq;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.Networking;
 using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
 
 public class ActionSwitchTracker : MonoBehaviour
 {
@@ -14,14 +12,16 @@ public class ActionSwitchTracker : MonoBehaviour
     private const string GFORM_URL =
         "https://docs.google.com/forms/d/e/1FAIpQLSfrklA_gmW2PxyYOF2whtSaFZVM5cg_jTHkmLNoZivKzaVSjw/formResponse";
 
-    // entry ids
     private const string ENTRY_ACTION_SWITCH_RATE = "entry.1249336548";
     private const string ENTRY_PLAYER_ID          = "entry.1290620746";
     private const string ENTRY_SCENE              = "entry.1425215553";
 
     private readonly List<int> seq = new List<int>();   // 0=gravity, 1=magnet
-    private string csvPath;
     private string playerId;
+
+    private string lastSceneSubmitted = null;
+    private float  lastSubmitAtRealtime = -999f;
+    [SerializeField] private float submitCooldownSec = 1.0f; 
 
     void Awake()
     {
@@ -29,17 +29,7 @@ public class ActionSwitchTracker : MonoBehaviour
         Instance = this;
         DontDestroyOnLoad(gameObject);
 
-        csvPath = Path.Combine(Application.dataPath, "ActionSwitchRate.csv");
         playerId = SystemInfo.deviceUniqueIdentifier;
-        EnsureHeader();
-    }
-
-    void EnsureHeader()
-    {
-        if (!File.Exists(csvPath))
-        {
-            File.WriteAllText(csvPath, "player_id,scene,N,switch_count,action_switch_rate,sequence\n");
-        }
     }
 
     public void RecordGravityFlip() { seq.Add(0); }
@@ -47,24 +37,25 @@ public class ActionSwitchTracker : MonoBehaviour
 
     public void SaveAndReset()
     {
+        string scene = SceneManager.GetActiveScene().name;
+        if (lastSceneSubmitted == scene &&
+            (Time.realtimeSinceStartup - lastSubmitAtRealtime) < submitCooldownSec)
+        {
+            seq.Clear();
+            return;
+        }
+
         int N = seq.Count;
         int switches = 0;
         if (N >= 2)
-        {
-            for (int i = 0; i < N - 1; i++)
-            {
-                if (seq[i] != seq[i + 1]) switches++;
-            }
-        }
+            for (int i = 0; i < N - 1; i++) if (seq[i] != seq[i + 1]) switches++;
+
         float rate = (N == 0) ? 0f : (float)switches / N;
 
-        string scene = SceneManager.GetActiveScene().name;
-        string sequenceStr = string.Join("|", seq.Select(i => i.ToString()).ToArray());
-
-        string line = $"{playerId},{scene},{N},{switches},{rate.ToString(CultureInfo.InvariantCulture)},{sequenceStr}\n";
-        File.AppendAllText(csvPath, line);
-
         StartCoroutine(UploadMetric(rate, playerId, scene));
+
+        lastSceneSubmitted = scene;
+        lastSubmitAtRealtime = Time.realtimeSinceStartup;
 
         seq.Clear();
     }
