2a7343242de537f23e3dc74c62746e2a8ea0c18e
Jiayi Zhang <zhangjiayi1512@outlook.com>
2025-11-20 18:34:04 -0800

    add key modify function

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class InputManager : MonoBehaviour
+{
+    public static InputManager _instance;
+    public Dictionary<string, KeyCode> keyMappings;
+    
+    public static InputManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<InputManager>();
+                if (_instance == null)
+                {
+                    GameObject go = new GameObject("InputManager");
+                    _instance = go.AddComponent<InputManager>();
+                }
+            }
+            return _instance;
+        }
+    }
+
+    void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+            keyMappings = new Dictionary<string, KeyCode>();
+            keyMappings.Add("SwitchGravity", KeyCode.Space);
+            keyMappings.Add("SwitchMagnetic", KeyCode.LeftShift);
+            keyMappings.Add("Respawn", KeyCode.R);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetKey(string actionName, KeyCode newKey)
+    {
+        if (keyMappings.ContainsKey(actionName))
+        {
+            keyMappings[actionName] = newKey;
+        }
+    }
+
+    public void ClearKeys(string actionName)
+    {
+        if (keyMappings.ContainsKey(actionName))
+        {
+            keyMappings[actionName] = KeyCode.None;
+        }
+    }
+}
--- Assets/Scripts/PlayerMagnetismControll.cs
+++ Assets/Scripts/PlayerMagnetismControll.cs
@@ -11,8 +11,20 @@ public class PlayerMagnetismControll : MonoBehaviour
     public Image energyBar;
     float lastSwitchTime = -Mathf.Infinity;
 
+    private InputManager inputManager;
+    private KeyBindUI keyBindUI;
+
+    void Start()
+    {
+        inputManager = InputManager.Instance;
+        keyBindUI = GetComponent<KeyBindUI>();
+    }
+
     void Update()
     {
+        if (keyBindUI != null && keyBindUI.isRebinding)
+            return;
+
         UpdatePole();
 
         if (forceFieldSwitchEnergy < forceFieldSwitchMaxEnergy)
@@ -51,7 +63,8 @@ public class PlayerMagnetismControll : MonoBehaviour
 
         if (magnetism == null) return;
 
-        if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift)) {
+        //if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift)) {
+        if (inputManager != null && Input.GetKeyDown(inputManager.keyMappings["SwitchMagnetic"])) {
             OperationAnalytics.Instance?.RegisterOperation();
             if (forceFieldSwitchEnergy < 1.0f)
                 return;
